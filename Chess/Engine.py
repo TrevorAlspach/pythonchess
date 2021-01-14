acd6f9b786b2cae63d55da74a18430513e4d128e
TrevorAlspach <60957308+TrevorAlspach@users.noreply.github.com>
2021-01-13 19:25:07 -0500

    backup before piece class is implemented

--- Chess/Engine.py
+++ Chess/Engine.py
@@ -3,6 +3,13 @@ Engine class is responsible for storing information about current state of the c
 moves and a move log
 """
 
+class Piece:
+    def __init__(self, color, type, can_take = False):
+        self.color = color
+        self.type = type
+        self.can_take = can_take
+
+
 
 class GameState:
     def __init__(self):
@@ -22,3 +29,45 @@ class GameState:
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+
+    def is_valid_move(self, move):
+        if self.whiteToMove:
+            if self.board[move.start_row][move.start_col] in ("bP","bR","bN","bB","bQ","bK"):
+                return False
+            if self.board[move.end_row][move.end_col] in ("wP","wR","wN","wB","wQ","wK"):
+                return False
+        else:
+            if self.board[move.start_row][move.start_col] in ("wP","wR","wN","wB","wQ","wK"):
+                return False
+            if self.board[move.end_row][move.end_col] in ("bP","bR","bN","bB","bQ","bK"):
+                return False
+        return True
+
+
+class Move():
+
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2,"7": 1, "8": 0}
+    rows_to_ranks = {v: k for k,v in ranks_to_rows.items()}
+    files_to_cols = {"a": 0, "b": 1,"c": 2,"d": 3, "e": 4,"f": 5, "g": 6, "h": 7}
+    cols_to_files = {v: k for k,v in files_to_cols.items()}
+
+    def __init__(self, start_square, end_square, board):
+        self.start_row = start_square[0]
+        self.start_col = start_square[1]
+        self.end_row = end_square[0]
+        self.end_col = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row,self.start_col) + self.get_rank_file(self.end_row,self.end_col)
+
+    def get_rank_file(self,r,c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
+
--- Chess/Main.py
+++ Chess/Main.py
@@ -2,8 +2,9 @@
 
 import pygame as pg
 from Chess import Engine
+import pdb
 
-WIDTH = HEIGHT = 720
+WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQ_SIZE = HEIGHT // DIMENSION
 MAX_FPS = 20
@@ -33,13 +34,37 @@ def main():
     game_state = Engine.GameState()
     load_images()
     running = True
+    sq_selected = ()  # keep track of the user's last click (row,column)
+    player_clicks = []  # Keep track of # of user clicks (two tuples: [(6,4),(4,4)])
 
     while running:
-        draw_game_state(screen,game_state)
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 running = False
+            elif e.type == pg.MOUSEBUTTONDOWN:
+                location = pg.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
 
+                if sq_selected == (row, col):  # user selected the same square twice
+                    sq_selected = ()
+                    player_clicks = []
+                else:
+                    sq_selected = (row, col)
+                    player_clicks.append(sq_selected)
+
+                if len(player_clicks) == 2:
+                    move = Engine.Move(player_clicks[0], player_clicks[1], game_state.board)
+                    if game_state.is_valid_move(move):
+                        print(move.get_chess_notation())
+                        game_state.makeMove(move)
+                    sq_selected = ()
+                    player_clicks = []
+
+
+
+
+        draw_game_state(screen, game_state)
         clock.tick(MAX_FPS)
         pg.display.flip()
 
@@ -52,11 +77,11 @@ def draw_game_state(screen, game_state):
 
 # Draws the squares on the board
 def draw_board(screen):
-    colors = [pg.Color( 232, 235, 239), pg.Color(125, 135, 150)]
+    colors = [pg.Color(232, 235, 239), pg.Color(125, 135, 150)]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            color = colors[((r+c) % 2)]
-            pg.draw.rect(screen, color, pg.Rect(c*SQ_SIZE, r*SQ_SIZE,SQ_SIZE, SQ_SIZE))
+            color = colors[((r + c) % 2)]
+            pg.draw.rect(screen, color, pg.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 # Draws the pieces on the board
@@ -65,9 +90,7 @@ def draw_pieces(screen, board):
         for c in range(DIMENSION):
             piece = board[r][c]
             if piece != '--':
-                screen.blit(IMAGES[piece],pg.Rect(c*SQ_SIZE, r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
-            else:
-                pass
+                screen.blit(IMAGES[piece], pg.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == "__main__":
